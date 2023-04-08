4284bcc12567f80e5779ef9e7356d2e0174dc235
ninjawit <80984772+ninjawit@users.noreply.github.com>
2023-04-07 21:57:29 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Generates a random number between 1-3, assigns a game choice
-// based on the number, then returns it
+// Generate a random number between 1-3, assign a game choice
+// based on the number, then return it
 function getComputerChoice() {
     let computerChoice;
     let randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -18,8 +18,42 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+// Prompt user to make their selection, then return as uppercase
 function getPlayerChoice() {
     let playerChoice = prompt("Rock, paper, or scissors?");
     return playerChoice.toUpperCase();
+}
+
+// Compares player and computer selection and determines winner
+function playRound(playerSelection, computerSelection) {
+    let result;
+    
+    if (playerSelection === "ROCK" && computerSelection === "ROCK") {
+        return result = "You tied! You both chose ROCK";
+    }
+    else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        return result = "You lose! PAPER beats ROCK";
+    }
+    else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return result = "You win! ROCK beats SCISSORS";
+    }
+    else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return result = "You win! PAPER beats ROCK";
+    }
+    else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
+        return result = "You tied! You both chose PAPER";
+    }
+    else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return result = "You lose! SCISSORS beats PAPER";
+    }
+    else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return result = "You lose! ROCK beats SCISSORS";
+    }
+    else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return result = "You win! SCISSORS beats PAPER";
+    }
+    else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
+        return result = "You tied! You both chose SCISSORS";
+    }
 }
 
