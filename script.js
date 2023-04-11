bed4fec2e3cbc25b6f56d83514501d6f2bc151a7
ninjawit <80984772+ninjawit@users.noreply.github.com>
2023-04-10 21:01:35 -0500

    Add game function and score counters within playRound function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let playerScore = 0;
+let computerScore = 0;
+
+game();
+
 // Generate a random number between 1-3, assign a game choice
-// based on the number, then return it
+// based on the number, then return the choice
 function getComputerChoice() {
     let computerChoice;
     let randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -24,7 +29,8 @@ function getPlayerChoice() {
     return playerChoice.toUpperCase();
 }
 
-// Compares player and computer selection and determines winner
+// Compare player vs computer choice, declare winner, then add 
+// 1 to score of winner
 function playRound(playerSelection, computerSelection) {
     let result;
     
@@ -32,24 +38,30 @@ function playRound(playerSelection, computerSelection) {
         return result = "You tied! You both chose ROCK";
     }
     else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        computerScore++;
         return result = "You lose! PAPER beats ROCK";
     }
     else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        playerScore++;
         return result = "You win! ROCK beats SCISSORS";
     }
     else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        playerScore++;
         return result = "You win! PAPER beats ROCK";
     }
     else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
         return result = "You tied! You both chose PAPER";
     }
     else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        computerScore++;
         return result = "You lose! SCISSORS beats PAPER";
     }
     else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        computerScore++;
         return result = "You lose! ROCK beats SCISSORS";
     }
     else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        playerScore++;
         return result = "You win! SCISSORS beats PAPER";
     }
     else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
@@ -57,3 +69,15 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+// run playRound five times and print updated score after each 
+// round
+function game() {   
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        
+        console.log(playRound(playerSelection, computerSelection));
+        
+        console.log(`Your score: ${playerScore}, Computer score: ${computerScore}`);
+    }
+}
