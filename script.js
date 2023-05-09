3766789780b453f95cec337e447154a083151ebc
ninjawit <80984772+ninjawit@users.noreply.github.com>
2023-05-08 20:34:56 -0500

    Add style, UI, and updated Javascript logic

--- script.js
+++ script.js
@@ -1,83 +1,90 @@
+let playerSelection;
+let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
-game();
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        getPlayerSelection(button);
+        getComputerSelection();
+        playRound(playerSelection, computerSelection);
+        firstToFive();
+    });
+});
 
-// Generate a random number between 1-3, assign a game choice
-// based on the number, then return the choice
-function getComputerChoice() {
-    let computerChoice;
+const results = document.querySelector("#results");
+
+const score = document.querySelector("#score");
+
+function getPlayerSelection(buttonClicked) {
+    playerSelection = buttonClicked.id;
+    return playerSelection;
+}
+
+function getComputerSelection() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
 
     switch(randomNumber) {
         case 1:
-            computerChoice = "ROCK";
+            computerSelection = "rock";
             break;
         case 2:
-            computerChoice = "PAPER";
+            computerSelection = "paper";
             break;
         case 3:
-            computerChoice = "SCISSORS";
+            computerSelection = "scissors";
             break;
     }
-    return computerChoice;
+    return computerSelection;
 }
 
-// Prompt user to make their selection, then return as uppercase
-function getPlayerChoice() {
-    let playerChoice = prompt("Rock, paper, or scissors?");
-    return playerChoice.toUpperCase();
-}
-
-// Compare player vs computer choice, declare winner, then add 
-// 1 to score of winner
 function playRound(playerSelection, computerSelection) {
-    let result;
-    
-    if (playerSelection === "ROCK" && computerSelection === "ROCK") {
-        return result = `You tied! You both chose ${playerSelection}`;
+    if (playerSelection === computerSelection) {
+        results.textContent = `You tied! You both chose ${playerSelection}`;
+        score.textContent = `Player: ${playerScore} \xa0\xa0\xa0 CPU: ${computerScore}`;
     }
-    else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore++;
-        return result = `You lose! ${computerSelection} beats ${playerSelection}`;
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        score.textContent = `Player: ${playerScore} \xa0\xa0\xa0 CPU: ${computerScore}`;
     }
-    else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++;
-        return result = `You win! ${playerSelection} beats ${computerSelection}`;
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        score.textContent = `Player: ${playerScore} \xa0\xa0\xa0 CPU: ${computerScore}`;
     }
-    else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++;
-        return result = `You win! ${playerSelection} beats ${computerSelection}`;
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        score.textContent = `Player: ${playerScore} \xa0\xa0\xa0 CPU: ${computerScore}`;
     }
-    else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
-        return result = `You tied! You both chose ${playerSelection}`;
-    }
-    else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore++;
-        return result = `You lose! ${computerSelection} beats ${playerSelection}`;
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        score.textContent = `Player: ${playerScore} \xa0\xa0\xa0 CPU: ${computerScore}`;
     }
-    else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore++;
-        return result = `You lose! ${computerSelection} beats ${playerSelection}`;
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        score.textContent = `Player: ${playerScore} \xa0\xa0\xa0 CPU: ${computerScore}`;
     }
-    else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore++;
-        return result = `You win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
-        return result = `You tied! You both chose ${playerSelection}`;
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        score.textContent = `Player: ${playerScore} \xa0\xa0\xa0 CPU: ${computerScore}`;
     }
 }
 
-// run playRound five times and print updated score after each 
-// round
-function game() {   
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        
-        console.log(playRound(playerSelection, computerSelection));
-        
-        console.log(`Your score: ${playerScore}, Computer score: ${computerScore}`);
+function firstToFive() {
+    if (playerScore == 5) {
+        results.textContent = "Congratulations, you won the game!";
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore == 5) {
+        results.textContent = "Sorry, you lost the game :(";
+        playerScore = 0;
+        computerScore = 0;
     }
 }
